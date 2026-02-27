26c3c14dd2ad338c6b081152a0ece694b0de88e1
Asmitha267 <asmithagourou@gmail.com>
2026-02-27 16:02:08 +0530

    UC4:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,12 +1,23 @@
 public class PalindromeCheckerApp {
-    static final String APP_NAME = "Palindrome Checker App";
-    static final String VERSION = "Version 1.0";
 
     public static void main(String[] args) {
-        System.out.println("Welcome to " + APP_NAME);
-        System.out.println(VERSION);
 
-        System.out.println("Application started successfully.");
-        System.out.println("Ready for next use case...");
+        // Original String
+        String original = "radar";
+
+        // Variable to store reversed string
+        String reversed = "";
+
+        // Reverse using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);   // String concatenation
+        }
+
+        // Compare using equals()
+        if (original.equals(reversed)) {
+            System.out.println("The string \"" + original + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + original + "\" is NOT a Palindrome.");
+        }
     }
 }
