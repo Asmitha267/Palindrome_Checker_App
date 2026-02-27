5cae4252ea3ca5b203dd0a2907daf63b89c5d1f3
Asmitha267 <asmithagourou@gmail.com>
2026-02-27 16:38:38 +0530

    U2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,49 +1,25 @@
-import java.util.Scanner;
-import java.util.Stack;
-import java.util.Queue;
-import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
 
-    // Method to check palindrome using Queue + Stack
-    public static boolean isPalindrome(String input) {
-
-        Stack<Character> stack = new Stack<>();       // LIFO
-        Queue<Character> queue = new LinkedList<>();  // FIFO
-
-        // Fill both data structures
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            stack.push(ch);   // push to stack
-            queue.add(ch);    // enqueue to queue
-        }
-
-        // Compare stack pop vs queue dequeue
-        while (!stack.isEmpty()) {
-            if (stack.pop() != queue.remove()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        // Hardcoded string (String Literal)
+        String word = "madam";
 
-        System.out.println("=== Palindrome Checker App (UC6: Queue + Stack Based) ===");
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
+        // Reverse the string
+        String reversed = "";
 
-        boolean result = isPalindrome(input);
-
-        if (result) {
-            System.out.println("Result: The given string is a palindrome.");
-        } else {
-            System.out.println("Result: The given string is NOT a palindrome.");
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
         }
 
-        scanner.close();
+        // Check palindrome using if-else
+        if (word.equals(reversed)) {
+            System.out.println("The word \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
+        }
+
+        System.out.println("Program executed successfully.");
     }
 }
